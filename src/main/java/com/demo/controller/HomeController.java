900d4d0d1181ab664a024bc7a392e5c0eaa809fd
jiangqiang <jiangqiang110@126.com>
2021-03-12 11:17:44 +0800

    demo

---
+++ src/main/java/com/demo/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.demo.controller;
+
+import com.jq.dbapi.DBApi;
+import com.jq.dbapi.entity.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @program: starterDemo
+ * @description:
+ * @author: jiangqiang
+ * @create: 2021-03-11 11:42
+ **/
+@Slf4j
+@RestController
+public class HomeController {
+
+    @Autowired
+    DBApi dbApi;
+
+    @RequestMapping("/hello")
+    public ResponseDto hello(@RequestBody Map<String,Object> map) {
+        ResponseDto execute = dbApi.execute(map, "getUser");
+        return execute;
+    }
+
+    @RequestMapping("/getUserIn")
+    public ResponseDto getUserIn(@RequestBody Map<String,Object> map) {
+        ResponseDto execute = dbApi.execute(map, "getUserIn");
+        return execute;
+    }
+}
