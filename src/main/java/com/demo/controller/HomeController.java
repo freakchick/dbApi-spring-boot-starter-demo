86b661e3de6d81e682d46353f3b538e0ad0fb0f1
jiangqiang <jiangqiang110@126.com>
2022-12-02 17:01:56 +0800

    1.1.0 demo

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.demo.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.gitee.freakchicken.DBApi;
-import com.gitee.freakchicken.entity.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,21 +20,42 @@ import java.util.Map;
  * @create: 2021-03-11 11:42
  **/
 @Slf4j
+@RequestMapping("/student")
 @RestController
 public class HomeController {
 
     @Autowired
     DBApi dbApi;
 
-    @RequestMapping("/hello")
-    public ResponseDto hello(@RequestBody Map<String,Object> map) {
-        ResponseDto execute = dbApi.execute(map, "getUser");
-        return execute;
+    @RequestMapping("/getAll")
+    public List<JSONObject> getAllStudent() {
+        List<JSONObject> jsonObjects = dbApi.executeQuery(null, "user", "getUser");
+        return jsonObjects;
     }
 
-    @RequestMapping("/getUserIn")
-    public ResponseDto getUserIn(@RequestBody Map<String,Object> map) {
-        ResponseDto execute = dbApi.execute(map, "getUserIn");
-        return execute;
+    @RequestMapping("/getById")
+    public List<JSONObject> getStudentById(Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        List<JSONObject> jsonObjects = dbApi.executeQuery(map, "user2", "getUserById");
+        return jsonObjects;
     }
+
+    @RequestMapping("/getById2")
+    public List<Student> getStudentById2(Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        List<Student> list = dbApi.executeQuery(map, "user2", "getUserById", Student.class);
+        return list;
+    }
+
+    @RequestMapping("/add")
+    public Integer add(String name, Integer age) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name);
+        map.put("age", age);
+        int i = dbApi.executeUpdate(map, "user2", "createStudent");
+        return i;
+    }
+
 }
